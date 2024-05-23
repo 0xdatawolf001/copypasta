a0b717e9bf3bd3b9ee915760578a8df0b79ac10f
Jerome <jeromeyue001@gmail.com>
2024-05-24 07:25:14 +0800

    revert

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
@@ -14,8 +12,7 @@ def extract_text_from_website(url):
 
 # Streamlit app
 def main():
-    st.title("Copy Pasta 🍝")
-    st.subheader("Paste Link, Get Text, One Click Copy. No more max main text highlighting")
+    st.title("Webpage Text Extractor and Copier")
     if 'extracted_text' not in st.session_state:
         st.session_state['extracted_text'] = ''
     url = st.text_input("Enter the URL of the website:")
@@ -32,23 +29,9 @@ def main():
         else:
             st.warning("Please enter a URL.")
 
-    if st.button("Copy to Clipboard"):
-        st.markdown(
-            f"""
-            <script>
-            function copyToClipboard(text) {{
-                navigator.clipboard.writeText(text).then(function() {{
-                    console.log('Text copied to clipboard');
-                }}, function(err) {{
-                    console.error('Could not copy text: ', err);
-                }});
-            }}
-            copyToClipboard(`{st.session_state['extracted_text']}`);
-            </script>
-            """,
-            unsafe_allow_html=True
-        )
-        st.success("Text copied to clipboard!")
+    if st.session_state['extracted_text']:
+        st.text_area("Copy the text below:", value=st.session_state['extracted_text'], height=400)
+        st.info("Select the text above and copy it manually.")
 
 if __name__ == "__main__":
     main()
