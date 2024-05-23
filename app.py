bcb2755816ef5367d24ba6c6a27bbba1e050a7fc
Jerome <jeromeyue001@gmail.com>
2024-05-24 07:12:07 +0800

    changed ux

--- app.py
+++ app.py
@@ -14,23 +14,47 @@ def extract_text_from_website(url):
 def main():
     st.title("Copy Pasta 🍝")
     st.subheader("Paste Link, Get Text, One Click Copy. No more max main text highlighting")
+    
     if 'extracted_text' not in st.session_state:
         st.session_state['extracted_text'] = ''
+    
+    if 'full_text' not in st.session_state:
+        st.session_state['full_text'] = ''
+    
     url = st.text_input("Enter the URL of the website:")
 
     if st.button("Extract Text"):
         if url:
             try:
                 st.session_state['extracted_text'] = extract_text_from_website(url)
-                st.text_area("Extracted Text:", value=st.session_state['extracted_text'], height=400)
+                st.session_state['full_text'] = st.session_state['extracted_text']
                 st.success("Text extraction successful!")
             except Exception as e:
                 st.error("An error occurred during text extraction.")
                 st.error(str(e))
         else:
             st.warning("Please enter a URL.")
-
+    
+    # Toggle button for prefix paragraph
+    add_prefix = st.checkbox("Add Prefix Paragraph")
+    
+    # Default prefix paragraph text
+    default_prefix = ("Extract the key insights and takeaways. Write in point form and organize section in headers. "
+                      "Make sure it is comprehensive and complete and you don’t lose out important information. "
+                      "At the end, have a call to action on the next steps based on what the write up suggests.")
+    
+    if add_prefix:
+        prefix_paragraph = st.text_area("Prefix Paragraph", value=default_prefix, height=150)
+    else:
+        prefix_paragraph = ""
+    
+    if st.button("Refresh Text To Copy"):
+        st.session_state['full_text'] = f"{prefix_paragraph}\n\n{st.session_state['extracted_text']}" if add_prefix else st.session_state['extracted_text']
+    
+    st.text_area("Full Text:", value=st.session_state['full_text'], height=400)
+    
     if st.button("Copy to Clipboard"):
+        full_text = st.session_state.get('full_text', '')
         st.markdown(
             f"""
             <script>
@@ -56,7 +80,7 @@ def main():
                     document.body.removeChild(textArea);
                 }}
             }}
-            copyToClipboard(`{st.session_state['extracted_text']}`);
+            copyToClipboard(`{full_text}`);
             </script>
             """,
             unsafe_allow_html=True
