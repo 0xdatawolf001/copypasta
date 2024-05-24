a966babc2f42392a5928c24208303c5f43c87909
Jerome <jeromeyue001@gmail.com>
2024-05-25 01:20:23 +0800

    fix pytessaract bug

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ import pytesseract
 import io
 from PyPDF2 import PdfReader
 from PIL import Image
+import shutil 
+import streamlit as st
+
+
 
 # Function to extract main body text from a URL
 def extract_text_from_url(url):
