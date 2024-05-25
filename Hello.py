19af1093731030eaf27016e6f20a360f581c1d6a
Jerome <jeromeyue001@gmail.com>
2024-05-25 21:45:03 +0800

    Added pages

---
+++ Hello.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Welcome to Copy Pasta 🍝")
+st.write("""
+         This is where you can extract text, copy them, and then use it for LLM prompting.
+         
+         I created this because its much easier to copy text at a go (copy to clipboard) as opposed to manually highlighting with your finger on the phone
+
+         Here are some pages that you may enjoy. This project is just a pet project I did hastily on the weekends. 
+
+         There are 2 tabs:
+         1. **Extract Text** allows you extract text from various media types: PDFs, websites, youtube, and images
+         2. **Marketing Prompts** allows you to take that extracted text and pass it into some pre-written marketing prompts that I wrote that can hopefully help you summarize complex writing into more business friendly terms
+
+         My Twitter account is: 0xDataWolf. Let me know if you have any problems or questions and I'll try to help
+
+         Let's go!
+         """)
+
